6e3f90325021cfb17c6ee3ffe3ecfe89690fb696
gabrielrodrig0 <gabrielrodrigonaga@gmail.com>
2025-04-03 22:25:25 -0300

    add: funcionalidade relatorio csv

--- src/main/java/br/com/pgm/ctec/uhscope/modules/relatorio/RelatorioService.java
+++ src/main/java/br/com/pgm/ctec/uhscope/modules/relatorio/RelatorioService.java
@@ -1,14 +1,16 @@
 package br.com.pgm.ctec.uhscope.modules.relatorio;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import br.com.pgm.ctec.uhscope.modules.afastamento.entities.AfastamentoEntity;
 import br.com.pgm.ctec.uhscope.modules.procuradores.ProcuradorRepository;
 import br.com.pgm.ctec.uhscope.modules.procuradores.entities.ProcuradorEntity;
 import br.com.pgm.ctec.uhscope.modules.utils.MethodsUtils;
-import br.com.pgm.ctec.uhscope.modules.utils.Relatorio;
 import jakarta.validation.ValidationException;
 
 @Service
@@ -20,13 +22,14 @@ public class RelatorioService {
     @Autowired
     MethodsUtils methodsUtils;
     
-    public String get(double valor, int mes, int ano) throws ValidationException{
+    
+    public String get(double valor, int mes, int ano) throws ValidationException {
         int anoAtual = LocalDate.now().getYear();
-        if(mes<1 || mes>12)
-        {
+        
+        if (mes < 1 || mes > 12) {
             throw new ValidationException("Esse mês não existe! Insira um mês que exista (1-12)");
         }
-        if(valor<=0){
+        if (valor <= 0) {
             throw new ValidationException("O valor não pode ser menor ou igual a zero.");
         }
         if (ano < 1900 || ano > anoAtual) {
@@ -34,32 +37,32 @@ public class RelatorioService {
         }
 
         String dataInicio = String.format("%02d/%02d/%d", 1, mes, ano);
+        String dataFim = String.format("%02d/%02d/%d", LocalDate.of(ano, mes, 1).lengthOfMonth(), mes, ano);
+
         ArrayList<ProcuradorEntity> procuradores = (ArrayList<ProcuradorEntity>) this.procuradorRepository.findAll();
-        ArrayList<Relatorio> relatorios = new ArrayList<>();
-    
+        StringBuilder resultado = new StringBuilder();
+
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.getDefault());
+        symbols.setDecimalSeparator(',');
+        DecimalFormat df = new DecimalFormat("00000.00", symbols);
+
         for (ProcuradorEntity procurador : procuradores) {
             List<AfastamentoEntity> afastamentos = new ArrayList<>(procurador.getAfastamentos());
 
             double uh = this.methodsUtils.calculaUHMensal(afastamentos, procurador, mes, ano);
-            
-            Relatorio relatorio = new Relatorio();
-            
-            relatorio.setCpf(procurador.getCpf());
-            relatorio.setMatricula(procurador.getMatricula());
-            relatorio.setNome(procurador.getNome());
-            relatorio.setUh(uh);
-            relatorios.add(relatorio);
-        }
-    
-        // Construindo a String com todos os relatórios formatados
-        StringBuilder resultado = new StringBuilder();
-        resultado.append("Relatórios para ").append(dataInicio).append("\n");
-        resultado.append("-------------------------------------------------\n");
+            System.out.println("\n");
+            System.out.println("Procurador: " + procurador.getMatricula() + " - UH: " + uh);
+            System.out.println("\n");
+            double valorFinal = (uh / 100) * valor;
 
-        for (Relatorio r : relatorios) {
-            resultado.append(String.format("Nome: %s | CPF: %s | Matrícula: %s | UH: %.1f\n",
-                    r.getNome(), r.getCpf(), r.getMatricula(), r.getUh()));
+            resultado.append(String.format("%s;HON SUCUMBENCIA;%s;%s;%s\n",
+                    procurador.getMatricula(),
+                    dataInicio,
+                    dataFim,
+                    df.format(valorFinal)
+            ));
         }
+
         return resultado.toString();
     }
     
